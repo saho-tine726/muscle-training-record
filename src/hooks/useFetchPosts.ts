858efbb3fd40b694c74c2d7ed274462aa759f2b6
saho-tine726 <sj_ej0726@outlook.jp>
2024-09-02 12:54:10 +0900

    リファクタリング/不要な記述を削除

--- src/hooks/useFetchPosts.ts
+++ src/hooks/useFetchPosts.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useRecoilValue } from 'recoil';
 import { userState } from "@/states/authState";
